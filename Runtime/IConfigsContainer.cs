7de537a7c0fa2b83c23127cdbc90653d9ebf2c7d
Miguel Tomas <game.gamester@gmail.com>
2020-02-26 01:03:19 +0000

    - Added *ISingleConfigContainer* - Added possibility to add single containers to *ConfigsProvider*

--- Runtime/IConfigsContainer.cs
+++ Runtime/IConfigsContainer.cs
@@ -12,4 +12,13 @@ namespace GameLovers.ConfigsContainer
 	{
 		List<T> Configs { get; }
 	}
+	
+	/// <summary>
+	/// Generic container of the unique single config imported with a ConfigsImporter script
+	/// The given <typeparamref name="T"/> type is the same of the config struct defined to be serialized in the scriptable object
+	/// </summary>
+	public interface ISingleConfigContainer<T> where T : struct
+	{
+		T Config { get; set; }
+	}
 }
